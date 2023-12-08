e50691e4976187701bb1557443efa980646950a0
student-akriti123 <akritisinha1602@gmail.com>
2023-12-08 19:05:57 +0530

    first commit

---
+++ basic star.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+ int a;
+ printf("Enter the value of a\n");
+ scanf("%d",&a);
+
+ int b;
+ printf("Enter the value of b\n");
+ scanf("%d",&b);
+
+ for(int i=1;i<=a;i++)
+    {
+        for(int j=0;j<=b;j++)
+        {
+          printf("*");
+        }
+        printf("\n");
+    }
+
+return 0;
+}
